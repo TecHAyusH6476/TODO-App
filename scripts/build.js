fecf01280ba67aebf54f90562415ca2db6f5933f
TecHAyusH6476 <ayushsingh6476@gmail.com>
2022-12-02 16:22:57 +0530

    updating components

--- scripts/build.js
+++ scripts/build.js
@@ -1,17 +1,15 @@
 'use strict';
 
-// Do this as the first thing so that any code reading it knows the right env.
+
 process.env.BABEL_ENV = 'production';
 process.env.NODE_ENV = 'production';
 
-// Makes the script crash on unhandled rejections instead of silently
-// ignoring them. In the future, promise rejections that are not handled will
-// terminate the Node.js process with a non-zero exit code.
+
 process.on('unhandledRejection', err => {
   throw err;
 });
 
-// Ensure environment variables are read.
+
 require('../config/env');
 
 const path = require('path');
@@ -31,7 +29,7 @@ const measureFileSizesBeforeBuild =
 const printFileSizesAfterBuild = FileSizeReporter.printFileSizesAfterBuild;
 const useYarn = fs.existsSync(paths.yarnLockFile);
 
-// These sizes are pretty large. We'll warn for bundles exceeding them.
+
 const WARN_AFTER_BUNDLE_GZIP_SIZE = 512 * 1024;
 const WARN_AFTER_CHUNK_GZIP_SIZE = 1024 * 1024;
 
@@ -40,16 +38,14 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
   process.exit(1);
 }
 
-// First, read the current file sizes in build directory.
-// This lets us display how much they changed later.
+
 measureFileSizesBeforeBuild(paths.appBuild)
   .then(previousFileSizes => {
-    // Remove all content but keep the directory so that
-    // if you're in it, you don't end up in Trash
+ 
     fs.emptyDirSync(paths.appBuild);
-    // Merge with the public folder
+   
     copyPublicFolder();
-    // Start the webpack build
+  
     return build(previousFileSizes);
   })
   .then(
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import * as classnames from 'classnames';
 import { inject, observer } from 'mobx-react';
-
+//importing 
 const TodoItem = inject('TodoStore')(observer(props => {
   const TodoStore = props.TodoStore;
 
